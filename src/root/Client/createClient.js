dd764159f8266cf1643678ad3f516a6e37076745
lamine785260388 <lamine785260388@gmail.com>
2022-12-07 02:18:04 +0000

    find or create client

--- src/root/Client/createClient.js
+++ src/root/Client/createClient.js
@@ -2,20 +2,36 @@ const { Client } = require('../../db/sequelize')
 const bcrypt=require('bcrypt')
   
 module.exports = (app) => {
-  app.post('/api/InsertClient', (req, res) => {
+  app.post('/api/InsertClient', async (req, res) => {
+    const client={
+      nom_client:req.body.nom_client,
+      prenom_client:req.body.prenom_client,
+      emailclient:req.body.emailclient,
+      phone_client:req.body.phone_client,
+      dateNaissance_client:req.body.dateNaissance_client,
+      lieuNaissance_client:req.body.lieuNaissance_client,
+      role:req.body.role,
+    }
 
   
-   Client.create({
-    nom_client:req.body.nom_client,
-    prenom_client:req.body.prenom_client,
-    emailclient:req.body.emailclient,
-    phone_client:req.body.phone_client,
-    dateNaissance_client:req.body.dateNaissance_client,
-    lieuNaissance_client:req.body.lieuNaissance_client,
-    role:req.body.role,
-
-    })
-    return res.json('issertion client success')
+    const [user, created] =  await Client.findOrCreate({
+      where: { nom_client: req.body.nom_client },
+      defaults: {
+        nom_client:req.body.nom_client,
+      prenom_client:req.body.prenom_client,
+      emailclient:req.body.emailclient,
+      phone_client:req.body.phone_client,
+      dateNaissance_client:req.body.dateNaissance_client,
+      lieuNaissance_client:req.body.lieuNaissance_client,
+      role:req.body.role,
+      }
+    });
+    if(created){
+      return res.json(user.id);
+    }
+    else{
+      return res.json(user.id)
+    }
     
    
    
